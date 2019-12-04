c6b77d89fc47442729c9ddbb8f4ab65a74f9ad2e
TheReconJacob <jvpokeyjake@gmail.com>
2019-12-04 16:55:00 +0000

    broken atm

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "sessions#index"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "session#destroy"
+
+  resources :users do
+    resources :links
+  end
 end
