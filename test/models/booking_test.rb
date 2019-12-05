e88f79612b7cc371e6cd317653cb82e403f7198a
Tommy Wray <Tommy_04@hotmail.co.uk>
2019-12-05 14:40:43 +0000

    added tests for the models

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class BookingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "bookings can have customers" do
+      booking = Booking.all.first
+      assert_equal(booking.user_id, 3) 
+    end
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  test "property should have a user" do
+  test "review should have a user" do
     review = Review.all.first
-    assert_equal(review.user_id, "3")
+    assert_equal(review.user.id, 3)
+  end
+  test "property should have a review" do
+    review = Review.all.first
+    assert_equal(review.booking.property.id, 1)
   end
 end
